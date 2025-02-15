c22afff73d3725e7a290cff19042a8823a7e9914
Ekhtiar Hossain <ekhtiaroct2002@gmail.com>
2025-02-15 12:49:39 +0600

    Third

--- script_nav_position.js
+++ script_nav_position.js
@@ -1,10 +1,18 @@
 
 window.addEventListener("scroll", function () {
     let navbar = document.querySelector(".navbar");
+    let logo = document.querySelector(".logo-name")
+    let acounts = document.querySelector(".acounts")
+
     if (window.scrollY > 0) {
         navbar.style.position = "fixed";
         navbar.style.top = "0";
+        logo.style.display = "flex"
+        acounts.style.display = "flex"
     } else {
         navbar.style.position = "relative";
+        logo.style.display = "none" 
+        acounts.style.display = "none"
     }
+
 });
